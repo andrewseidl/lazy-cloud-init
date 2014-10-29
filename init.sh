d5982452549626cb449e38da8415ec2a7878fa69
Andrew Seidl <dev@aas.io>
2014-10-29 03:10:48 -0500

    move common functions into init

--- init.sh
+++ init.sh
@@ -28,6 +28,19 @@ detectDistro() {
   readonly DIST
 }
 
+# Begin common functions
+getKeys() {
+  KEYURL=$1
+  PREF=${2:=$HOME}
+  if [ ! -d $PREF/.ssh ] ; then
+    mkdir $PREF/.ssh
+    chmod 600 $PREF/.ssh
+  fi
+
+  curl "$KEYURL" >> $PREF/.ssh/authorized_keys
+  chmod 600 $PREF/.ssh/authorized_keys
+}
+
 detectDistro
 DISTSH="$DIST.sh"
 
