dbebd0ca1a22d3a311178fde48650ada83c631b9
Andrew Seidl <dev@aas.io>
2014-10-29 02:53:45 -0500

    add debug flag, default variables

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+DEBUG=${DEBUG:=true}
+GHUSER=${GHUSER:="andrewseidl"}
+KEYURL=${KEYURL:="https://github.com/$GHUSER.keys"}
+REPOURL="https://raw.githubusercontent.com/andrewseidl/lazy-cloud-init/master/"
+
 # Figure out our distro
 detectDistro() {
   OS=`uname`
@@ -24,4 +29,15 @@ detectDistro() {
 }
 
 detectDistro
-echo $DIST
+DISTSH="$DIST.sh"
+
+if [ $DEBUG != true ] ; then
+  curl -O $REPOURL/$DISTSH
+  chmod +x $DISTSH
+fi
+
+source $DISTSH
+
+init
+
+echo $KEYURL
